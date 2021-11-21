090ebe1c87c5793bb914d0d83bc4e7c62fc99563
xytpai <xytpai@foxmail.com>
2021-11-21 14:21:53 +0800

    init

--- server.py
+++ server.py
@@ -1,17 +1,38 @@
 import pymysql as sql
 
-host = 'localhost'
-user = 'debian-sys-maint'
-password = 'qqdWvUpyYdfW9crD'
+info = {'host' : 'localhost',
+'user' : 'debian-sys-maint',
+'password' : 'qqdWvUpyYdfW9crD'
+}
 
-conn = sql.connect(
-    host=host,
-    user=user,
-    password=password,
-    charset='utf8')
-cursor = conn.cursor()
-cursor.execute("show databases;")
-print(cursor.fetchall())
-cursor.execute("show databases;")
-print(cursor.fetchall())
-conn.close()
+# def get_data():
+#     conn = sql.connect(
+#         host=info['host'],
+#         user=info['user'],
+#         password=info['password'],
+#         charset='utf8')
+#     cursor = conn.cursor()
+#     cursor.execute("show databases;")
+#     datas = cursor.fetchall()
+#     datas =str(datas)
+#     conn.close()
+#     return datas
+from flask import Flask, render_template, send_file
+from flask_login import LoginManager
+app = Flask(__name__)
+app.secret_key = 'abc'
+login = LoginManager()
+login.init_app(app)
+login.login_view = 'login'
+
+
+@app.route('/<path:name>', methods=['GET'])
+def get_path(name):
+    return name
+
+# @app.route('/', methods=['GET'])
+# def get_index():
+#     return get_data()
+
+if __name__=='__main__':
+	app.run(debug=True,host="0.0.0.0")
